d73eee971a4993e59ca515e3cb768c65f4fbe9e0
joaooliveiradev <joaooliveira1775@gmail.com>
2021-12-19 10:49:49 -0300

    adding theme to the themeProvider

---
+++ src/resources/theme.ts
@@ -0,0 +1,13 @@
+export const Theme = {
+  colors: {
+    white: '#FFFFFF',
+    blue: '#0099FF',
+    lightBlue: '#EEF6FF',
+    purple: '#1C214A',
+    darkPurple: '#161A3A',
+    gray: '#888888',
+    lightGray: '#AAAAAA',
+    black: '#444444',
+    green: '#3BE305'
+  }
+}
