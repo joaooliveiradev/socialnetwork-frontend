6fdb5f8d6a220ac09e0a4459b5f9898e6f98e645
joaooliveiradev <joaooliveira1775@gmail.com>
2021-12-22 10:08:40 -0300

    refactor some line on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-
+import { Root } from './root'
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  <StrictMode>
+    <Root />
+  </StrictMode>,
+  document.querySelector('[data-js="app"]'),
 )
