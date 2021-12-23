f8ead0807733778354a15a608c84f2d8a6f430df
joaooliveiradev <joaooliveira1775@gmail.com>
2021-12-23 12:40:54 -0300

    removing css import that never be used

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,4 @@
-import { css, ThemeProvider, createGlobalStyle } from 'styled-components/macro'
+import { ThemeProvider, createGlobalStyle } from 'styled-components/macro'
 import { App } from './app'
 import { theme } from './resources/theme'
 import 'normalize.css'
