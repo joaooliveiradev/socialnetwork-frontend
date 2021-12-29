8b06386b22da54b02db4de86a1a33134624bd915
joaooliveiradev <joaooliveira1775@gmail.com>
2021-12-29 12:24:47 -0300

    create input component

---
+++ src/ui/Input/input.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+type InputProps = {
+  className?: string,
+  type: string,
+  htmlFor: string,
+  labelText: string,
+  required: boolean,
+  placeholder: string,
+}
+const Input = ({ className, type, htmlFor, labelText, placeholder, required }: InputProps) => (
+  <label htmlFor={htmlFor}>
+    {labelText}
+    <input className={className} type={type} required={required} placeholder={placeholder} />
+  </label>
+)
+const StyledInput = styled(Input)`
+  height: 5rem;
+  font-size: 1.8rem;
+  border: solid 1px #DADADA;
+  border-radius: 6px;
+`
+export { StyledInput as Input }
