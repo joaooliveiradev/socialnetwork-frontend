98a8ad2f2b8c985c94b2f65762554ad832e38942
joaooliveiradev <joaooliveira1775@gmail.com>
2021-12-29 13:00:09 -0300

    adding tipography component

---
+++ src/ui/Tipography/tipography.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components/macro'
+type validHeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+type TitleProps = {
+  headingLevel: validHeadingLevel,
+  children: string,
+  className?: string,
+}
+const Tipography = ({ headingLevel, className, children }: TitleProps) => {
+  const Title = `${headingLevel}` as validHeadingLevel
+  return <Title className={className}>{children}</Title>
+}
+const TipographyStyled = styled(Tipography)`
+  font-size: 1.8rem;
+  color: ${({ theme }) => theme.colors.darkPurple};
+`
+export { TipographyStyled as Tipography }
