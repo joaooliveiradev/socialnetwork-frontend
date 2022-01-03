9a7fc0971e64f6425c763661b6c4273dcacb0956
joaooliveiradev <joaooliveira1775@gmail.com>
2022-01-03 15:11:37 -0300

    adding lock,user,mail svg to the component

--- src/shared/signup/signup.tsx
+++ src/shared/signup/signup.tsx
@@ -4,6 +4,7 @@ import { Input } from 'ui/input'
 import { Button } from 'ui/button'
 import { theme } from 'resources/theme'
 import { Link } from 'react-router-dom'
+import { UserSVG, MailSVG, LockSVG } from 'ui/icons'
 type SignUpProps = {
   className?: string
 }
@@ -11,9 +12,9 @@ const SignUp = ({ className }: SignUpProps) => (
   <form className={className}>
     <Tipography headingLevel="h1">Create Account</Tipography>
     <Tipography headingLevel="h3">Create account to start using SocialNetwork</Tipography>
-    <Input htmlFor="name" type="input" labelText="Full Name" placeholder="John Doe" required={true} />
-    <Input htmlFor="email" type="email" labelText="Email address" placeholder="name@domain.com" required={true} />
-    <Input htmlFor="password" type="password" labelText="Password" placeholder="at least 8 characters" required={true} />
+    <Input htmlFor="name" svg={<UserSVG />} type="input" labelText="Full Name" placeholder="John Doe" required={true} />
+    <Input htmlFor="email" svg={<MailSVG />} type="email" labelText="Email address" placeholder="name@domain.com" required={true} />
+    <Input htmlFor="password" svg={<LockSVG />} type="password" labelText="Password" placeholder="at least 8 characters" required={true} />
     <Button width="100%" height="5rem" bgColor={theme.colors.blue}>Sign Up</Button>
     <Tipography headingLevel="p">Already have an account? <Link to="/">Login</Link></Tipography>
   </form>
@@ -36,6 +37,7 @@ const StyledSignUp = styled(SignUp)`
     margin: 0;
   }
   label{
+    position: relative;
     color: ${({ theme }) => theme.colors.darkPurple};
     font-size: 2rem;
     display: flex;
@@ -44,7 +46,7 @@ const StyledSignUp = styled(SignUp)`
     margin-top: 2rem;
   }
   input {
-    padding-left: 2rem;
+    padding-left: 5rem;
   }
   input::-webkit-input-placeholder { /* WebKit, Blink, Edge */
     color: rgba(22, 26, 58, 0.7);
@@ -76,6 +78,12 @@ const StyledSignUp = styled(SignUp)`
   p {
     padding: 2rem 0rem;
     margin: 0;
+  }
+  svg {
+    position: absolute;
+    top: 3rem;
+    left: 1.5rem;
+    width: 2.6rem;
   }
 `
 export { StyledSignUp as SignUp }
