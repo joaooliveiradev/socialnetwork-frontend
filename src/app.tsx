7e28244c3e3ca54a02fd7a55536924f31d463676
joaooliveiradev <joaooliveira1775@gmail.com>
2022-01-03 11:42:03 -0300

    adding route to signup form

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { Homepage } from 'pages/homepage'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from 'shared/login'
+import { SignUp } from 'shared/signup'
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage formElement={<Login />} />} />
+      <Route path="/signup" element={<Homepage formElement={<SignUp />} />} />
     </Routes>
   )
 }
