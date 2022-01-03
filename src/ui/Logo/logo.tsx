c9acc7c9dac277a9f00fcefac531dbcb1c22a904
joaooliveiradev <joaooliveira1775@gmail.com>
2022-01-03 11:18:16 -0300

    deleted files

--- src/ui/Logo/logo.tsx
+++
@@ -1,9 +0,0 @@
-import { LogoSVG } from 'ui/icons'
-type logoProps = {
-  width: string,
-  height: string,
-}
-const Logo = ({ width, height }: logoProps) => (
-  <LogoSVG width={width} height={height}/>
-)
-export { Logo }
