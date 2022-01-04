7d9981912003ca6a246bc5d07a7d08fcd9f3fc08
joaooliveiradev <joaooliveira1775@gmail.com>
2022-01-04 12:15:30 -0300

    refactor to an inline style

--- src/shared/login/login.tsx
+++ src/shared/login/login.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import { Input } from 'ui/input'
 import { Tipography } from 'ui/tipography'
 import { Button } from 'ui/button'
-import { theme } from 'resources/theme'
 import { Link } from 'react-router-dom'
 import { MailSVG, LockSVG } from 'ui/icons'
 type FormLoginProps = {
@@ -15,7 +14,7 @@ const Login = ({ className }: FormLoginProps) => (
     <Input htmlFor="email" svg={<MailSVG />} labelText="Your email" type="email" placeholder="name@domain.com" required />
     <Input htmlFor="password" svg={<LockSVG />} labelText="Password" type="password" placeholder="at least 8 characters" required />
     <Link className="forgot" to="/forgot">Forgot password?</Link>
-    <Button width="100%" height="5rem" bgColor={theme.colors.blue}>Login</Button>
+    <Button width="100%" height="5rem" bgColor="#0099FF">Login</Button>
     <Tipography headingLevel="p">Not registered yet? <Link to="/signup">Create an Account</Link></Tipography>
   </form>
 )
