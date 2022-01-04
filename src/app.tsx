c730b3fcac77c2e9fd81cdc8930cad64c4f5805d
joaooliveiradev <joaooliveira1775@gmail.com>
2022-01-04 12:17:54 -0300

    adding forgot password route

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import { Homepage } from 'pages/homepage'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from 'shared/login'
 import { SignUp } from 'shared/signup'
+import { Forgot } from 'shared/forgot'
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage formElement={<Login />} />} />
       <Route path="/signup" element={<Homepage formElement={<SignUp />} />} />
+      <Route path="/forgot" element={<Homepage formElement={<Forgot />} />} />|
     </Routes>
   )
 }
