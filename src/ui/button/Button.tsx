6dd74a0fed154f3275cdcd6b92178eda00aaa0db
joaooliveiradev <joaooliveira1775@gmail.com>
2022-01-08 12:56:44 -0300

    editing button component to receive React elements too

--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -1,14 +1,15 @@
+import { ReactNode } from 'react'
 import styled from 'styled-components/macro'
 type ButtonProps = {
-    children: string;
-    width: string,
-    height: string,
-    bgColor: string,
+    children: ReactNode;
+    width?: string,
+    height?: string,
+    bgColor?: string,
 }
 const Button = styled.button<ButtonProps>`
-  background-color: ${props => props.bgColor};
+  background-color: ${props => props.bgColor !== undefined ? props.bgColor : '#0099FF'};
   width: ${props => props.width !== undefined ? props.width : '100%'};
-  height: ${props => props.height !== undefined ? props.height : '4rem'};
+  height: ${props => props.height !== undefined ? props.height : '5rem'};
   border: none;
   border-radius: 0.6rem;
   font-size: 1.8rem;
